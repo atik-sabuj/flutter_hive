323f3456381d7a69e98d553b1280e647112481fb
atik-sabuj <sabu235@gmail.com>
2023-12-23 17:04:11 +0600

    Hiva Box Fetching Data and Show Data

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,6 +20,20 @@ class _HomePageState extends State<HomePage> {
         children: [
 
         ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async{
+          
+          var box = await Hive.openBox('Sabuj Atik');
+
+          box.put('name', 'Sabuj Atik');
+          box.put('age', '27');
+
+          print(box.get('name'));
+          print(box.get('age'));
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
