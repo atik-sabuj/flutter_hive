7fd9ffc93f7bab68576e440c6c7011058ce52c57
atik-sabuj <sabu235@gmail.com>
2023-12-23 16:56:58 +0600

    Hiva Pkgs and Directory added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hive/home_page.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  var directory = await getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
+
   runApp(const MyApp());
 }
 
