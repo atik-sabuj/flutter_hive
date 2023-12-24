ced65331ea9517157ace3690d3d7c6872f2ce8f0
Atikuzzaman Sabuj <113611885+atik-sabuj@users.noreply.github.com>
2023-12-24 11:30:37 +0600

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -29,12 +29,19 @@ class _HomePageState extends State<HomePage> {
           var data = box.values.toList().cast<NotesModel>();
           return ListView.builder(
             itemCount: box.length,
+              reverse: true,
               itemBuilder: (context, index){
                 return Card(
-                  child: Column(
-                    children: [
-                      Text(data[index].title.toString())
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10 ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(data[index].title.toString()),
+                        Text(data[index].description.toString()),
+                      ],
+                    ),
                   ),
                 );
               }
